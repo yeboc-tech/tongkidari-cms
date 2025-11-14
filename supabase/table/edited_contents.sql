bb5367e0be2b22887b39016a530eebf14d987db1
Doyoon Kim <asfrom30@gmail.com>
2025-11-14 16:55:56 +0900

    fix

--- supabase/table/edited_contents.sql
+++ supabase/table/edited_contents.sql
@@ -1,19 +1,18 @@
-create table public.accuracy_rate (
-  problem_id text not null,
-  correct_answer text null,
-  difficulty text null,
-  score integer null,
-  accuracy_rate numeric(5, 2) null,
-  selection_rates jsonb null,
+create table public.edited_contents (
+  resource_id text not null,
+  json jsonb null,
   created_at timestamp with time zone null default now(),
   updated_at timestamp with time zone null default now(),
-  constraint accuracy_rate_pkey primary key (problem_id)
+  base64 text null,
+  constraint edited_contents_pkey primary key (resource_id)
 ) TABLESPACE pg_default;
 
-create index IF not exists idx_accuracy_rate_difficulty on public.accuracy_rate using btree (difficulty) TABLESPACE pg_default;
+create index IF not exists idx_edited_contents_created_at on public.edited_contents using btree (created_at) TABLESPACE pg_default;
 
-create index IF not exists idx_accuracy_rate_accuracy on public.accuracy_rate using btree (accuracy_rate) TABLESPACE pg_default;
+create index IF not exists idx_edited_contents_updated_at on public.edited_contents using btree (updated_at) TABLESPACE pg_default;
 
-create trigger update_accuracy_rate_updated_at BEFORE
-update on accuracy_rate for EACH row
+create index IF not exists idx_edited_contents_json on public.edited_contents using gin ("json") TABLESPACE pg_default;
+
+create trigger update_edited_contents_updated_at BEFORE
+update on edited_contents for EACH row
 execute FUNCTION update_updated_at_column ();
